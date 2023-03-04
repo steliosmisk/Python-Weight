a9d148beb9fdae97eed96fb23b6ecebe03ea82b2
Stelios Miskedakis <123480703+steliosmisk@users.noreply.github.com>
2023-03-04 09:07:29 +0200

    Update weight.py

--- weight.py
+++ weight.py
@@ -4,22 +4,27 @@ print("[2] LB to KG")
 print("[3] EXIT")
 
 
-def kilogram(weight):
-    return weight // 0.45359237
+def kilogram_to_pounds(weight):
+    return weight / 0.45359237
 
 
-def pounds(weight):
+def pounds_to_kilogram(weight):
     return weight * 0.45359237
 
 
 user_choice = input("Enter your choice: ")
 while user_choice in ['1', '2'] and user_choice != '3':
-    if user_choice == '1':
-        weight = float(input("Enter your weight [kg]: "))
-        total = kilogram(weight)
-    else:
-        weight = float(input("Enter your weight [lb]: "))
-        total = pounds(weight)
-    print(f"KG: {weight} | LB: {total}")
-    break
-
+    try:
+        if user_choice == '1':
+            weight = float(input("Enter your weight [kg]: "))
+            total = kilogram_to_pounds(weight)
+            print(f"KG: {weight} | LB: {total}")
+        elif user_choice == '2':
+            weight = float(input("Enter your weight [lb]: "))
+            total = pounds_to_kilogram(weight)
+            print(f"LB: {weight} | KG: {total}")
+        else:
+            print("Invalid input. Please enter 1, 2, or 3.")
+        user_choice = input("Enter your choice: ")
+    except ValueError:
+        print("Please enter a valid input.")
